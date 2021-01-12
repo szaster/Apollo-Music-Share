7664828b8695575d628707d4c1ca895ea07e1ef7
Svitlana Zaster <szaster@protonmail.com>
2021-01-11 23:48:22 -0600

    added styling to dialog window

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -11,21 +11,45 @@ import {
 } from "@material-ui/core";
 import { Link, AddBoxOutlined } from "@material-ui/icons";
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+  urlInput: {
+    margin: theme.spacing(1),
+  },
+  addSongButton: {
+    margin: theme.spacing(1),
+  },
+  dialog: {
+    textAlign: "center",
+  },
+  thumbnail: {
+    width: "90%",
+  },
+}));
+
 function AddSong() {
+  const classes = useStyles();
   const [dialog, setDialog] = React.useState(false);
 
   function handleCloseDialog() {
     setDialog(false);
   }
   return (
-    <div>
-      <Dialog open={dialog} onClose={handleCloseDialog}>
+    <div className={classes.container}>
+      <Dialog
+        className={classes.dialog}
+        open={dialog}
+        onClose={handleCloseDialog}
+      >
         <DialogTitle> Edit Song </DialogTitle>
         <DialogContent>
           <img
             alt="song thumbnail"
-            width="100%"
             src="https://artist.com/art-recognition-and-education/wp-content/themes/artist-blog/media-files/2016/05/abstract-2.jpg"
+            className={classes.thumbnail}
           />
           <TextField margin="dense" name="title" label="Title" fullWidth />
           <TextField margin="dense" name="artist" label="Artist" fullWidth />
@@ -40,19 +64,24 @@ function AddSong() {
           <Button onClick={handleCloseDialog} color="secondary">
             Cancel
           </Button>
-          <Button variant="outlined" color="primary">
+          <Button
+            className={classes.addSongButton}
+            variant="outlined"
+            color="primary"
+          >
             Add Song
           </Button>
         </DialogActions>
       </Dialog>
       <TextField
+        className={classes.urlInput}
         placeholder="Add Youtube or Soundcloud Url"
         fullWidth
         margin="normal"
         type="url"
         InputProps={{
           startAdornment: (
-            <InputAdornment>
+            <InputAdornment position="start">
               <Link />
             </InputAdornment>
           ),
