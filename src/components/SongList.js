7e06fd968f8f9475804d051101384ba3bb788593
Svitlana Zaster <szaster@protonmail.com>
2021-01-12 00:28:31 -0600

    added unformatted song list with icons

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,74 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
+import { PlayArrow, Save } from "@material-ui/icons";
 import React from "react";
 
 function SongList() {
-  return <div>Song list</div>;
+  //creating loading spinner
+  let loading = false;
+
+  const song = {
+    title: "bla",
+    artist: "blabla",
+    thumbnail:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FxiXLtEi4Ui8%2Fmaxresdefault.jpg&f=1&nofb=1",
+  };
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {Array.from({ length: 10 }, () => song).map((song, i) => (
+        <Song key={i} song={song} />
+      ))}
+    </div>
+  );
+}
+
+function Song({ song }) {
+  const { thumbnail, title } = song;
+  return (
+    <Card>
+      <div>
+        <CardMedia image={thumbnail} />
+        <div>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body1" component="p" color="textSecondary">
+              {title}
+            </Typography>
+            <IconButton size="small" color="primary">
+              <PlayArrow />
+            </IconButton>
+            <IconButton size="small" color="secondary">
+              <Save color="secondary" />
+            </IconButton>
+          </CardContent>
+        </div>
+      </div>
+    </Card>
+  );
 }
 
 export default SongList;
